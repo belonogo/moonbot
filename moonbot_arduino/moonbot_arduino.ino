75c21ac79a97fce88084849f88ff4ce796be29cb
KirillKhalin <139039682+KirillKhalin@users.noreply.github.com>
2023-07-16 14:22:31 +0300

    Update moonbot_arduino.ino

--- moonbot_arduino/moonbot_arduino.ino
+++ moonbot_arduino/moonbot_arduino.ino
@@ -1,7 +1,9 @@
 #include <ros.h> 
 #include <std_msgs/Empty.h> 
-#include <geometry_msgs/Twist.h> 
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3.h>
 #include <std_msgs/String.h> 
+#include <TroykaIMU.h> 
 #define ROBOT_WIDTH 150 
  
 int pot; 
@@ -19,15 +21,22 @@ int out;
 #define EN2 A1 
  
 int pwm = 128; 
-int command = 0; 
+int command = 0;
+
+Gyroscope gyroscope;
+
+geometry_msgs::Vector3 imuData;
+std_msgs::String str_msg;
+String data;
+
+ros::Publisher chatter("chatter", &str_msg);
+ros::Publisher imuDataPublisher("imu_data", &imuData);
  
 class NewHardware : public ArduinoHardware 
 { 
   public: 
     NewHardware(): ArduinoHardware(&Serial, 115200) {}; 
 }; 
-
-//dsksagf
   
 ros::NodeHandle_<NewHardware>  nh; 
   
@@ -73,24 +82,22 @@ void motor2SetSpeed(int spd1 = 0) {
   } 
 } 
 
-String data;
-
 void messageCb(const geometry_msgs::Twist& cmd_vel)  
 {  
     long lasttim=millis(); 
     double vel_x=cmd_vel.linear.x; 
     double vel_th=cmd_vel.angular.z; 
-    double left_vel=(vel_x-vel_th*ROBOT_WIDTH/2000)*255; 
-    double right_vel=(vel_x+vel_th*ROBOT_WIDTH/2000)*255; 
+    double left_vel=(vel_x-vel_th*ROBOT_WIDTH/2000)*255*3; 
+    double right_vel=(vel_x+vel_th*ROBOT_WIDTH/2000)*255*3;
+    left_vel = min(160, left_vel);
+    right_vel = min(160, right_vel);
     motor1SetSpeed((int)left_vel); 
     motor2SetSpeed((int)right_vel);
-    data = String(left_vel) + " " + String(right_vel);
-}  
- 
-ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb ); 
-std_msgs::String str_msg; 
-ros::Publisher chatter("chatter", &str_msg); 
- 
+    data = String(left_vel) + "    " + String(right_vel);
+}
+
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
+
 void setup() { 
   pinMode(BIN1, OUTPUT); 
   pinMode(AIN1, OUTPUT); 
@@ -102,15 +109,24 @@ void setup() {
  
   pinMode(A0, OUTPUT); 
   pinMode(A1, OUTPUT); 
-  Serial3.begin(9600); 
+  Serial3.begin(9600);
+
+  gyroscope.begin();
  
   nh.initNode();  
   nh.subscribe(sub); 
-  nh.advertise(chatter); 
-} 
+  nh.advertise(chatter);
+  nh.advertise(imuDataPublisher);
+}
+
 void loop() { 
   str_msg.data = data.c_str();
-  chatter.publish( &str_msg ); 
-  nh.spinOnce();  
+  imuData.x = gyroscope.readRotationDegX();
+  imuData.y = gyroscope.readRotationDegY();
+  imuData.z = gyroscope.readRotationDegZ();
+
+  chatter.publish(&str_msg);
+  imuDataPublisher.publish(&imuData);
+  nh.spinOnce(); 
   delay(1);  
 }
