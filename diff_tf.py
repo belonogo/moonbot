f968c2164e9b69ccda7f8ad3ba5bb7501ced175d
KirillKhalin <139039682+KirillKhalin@users.noreply.github.com>
2023-07-16 17:05:41 +0300

    Update diff_tf.py

--- diff_tf.py
+++ diff_tf.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python
 
 import rospy
+#import roslib
+#roslib.load_manifest('differential_drive')
 from math import sin, cos, pi
+
 from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import Twist
+
 from geometry_msgs.msg import Vector3
+
 from nav_msgs.msg import Odometry
 import tf
 from tf.broadcaster import TransformBroadcaster
 from std_msgs.msg import Int16, Int32, Int64, UInt32
 
 class DiffTf:
-    def init(self):
+    def __init__(self):
         rospy.init_node("diff_tf")
         self.nodename = rospy.get_name()
         rospy.loginfo("-I- %s started" % self.nodename)
+        
+        #### parameters #######
 
-        self.rate = 10.0
-        self.ticks_meter = float(1800)  # The number of wheel encoder ticks per meter of travel
-        self.base_width = float(0.15) # The wheel base width in meters
+        #Wheel radius : 0.032
+        # wheel circum = 2* 3.14 * 0.0325 = 0.2010
+        # One rotation encoder ticks : 9 ticks
+        # For 1 meter: 9 * ( 1 / 0.2010) = 48 ticks
+
+        self.rate = rospy.get_param('~rate',10.0)  # the rate at which to publish the transform
+        self.ticks_meter = float(rospy.get_param('ticks_meter', 190))  # The number of wheel encoder ticks per meter of travel
+        self.base_width = float(rospy.get_param('~base_width', 0.11)) # The wheel base width in meters
         
         self.base_frame_id = rospy.get_param('~base_frame_id','base_link') # basefootprint /the name of the base frame of the robot
         self.odom_frame_id = rospy.get_param('~odom_frame_id', 'odom') # the name of the odometry reference frame
         
-        self.encoder_min = 0
-        self.encoder_max = 4294967295
-        self.encoder_low_wrap = (self.encoder_max - self.encoder_min) * 0.3 + self.encoder_min
-        self.encoder_high_wrap = (self.encoder_max - self.encoder_min) * 0.7 + self.encoder_min
+        self.encoder_min = rospy.get_param('encoder_min', -2147483648)
+        self.encoder_max = rospy.get_param('encoder_max', 2147483648)
+        self.encoder_low_wrap = rospy.get_param('wheel_low_wrap', (self.encoder_max - self.encoder_min) * 0.3 + self.encoder_min )
+        self.encoder_high_wrap = rospy.get_param('wheel_high_wrap', (self.encoder_max - self.encoder_min) * 0.7 + self.encoder_min )
  
         self.t_delta = rospy.Duration(1.0/self.rate)
         self.t_next = rospy.Time.now() + self.t_delta
@@ -108,3 +120,76 @@ class DiffTf:
                 
             # publish the odom information
             quaternion = Quaternion()
+
+
+            quaternion.x = 0.0
+            quaternion.y = 0.0
+
+            quaternion.z = sin( self.th / 2 )
+            quaternion.w = cos( self.th / 2 )
+
+            self.odomBroadcaster.sendTransform(
+                (self.x, self.y, 0),
+                (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
+                rospy.Time.now(),
+                self.base_frame_id,
+                self.odom_frame_id
+                )
+            
+            odom = Odometry()
+            odom.header.stamp = now
+            odom.header.frame_id = self.odom_frame_id
+            odom.pose.pose.position.x = self.x
+            odom.pose.pose.position.y = self.y
+            odom.pose.pose.position.z = 0
+            odom.pose.pose.orientation = quaternion
+            odom.child_frame_id = self.base_frame_id
+            odom.twist.twist.linear.x = self.dx
+            odom.twist.twist.linear.y = 0
+            odom.twist.twist.angular.z = self.dr
+            self.odomPub.publish(odom)
+            
+    def imu_value_update(self, imu_data):
+        orient = imu_data
+
+        self.yaw = orient.x
+        self.pitch = orient.y
+        self.roll = orient.z
+
+        try:	
+            self.quaternion_1 = tf.transformations.quaternion_from_euler(self.yaw, self.pitch, self.roll)
+
+        except:
+            rospy.logwarn("Unable to get quaternion values")
+            pass
+
+    def lwheelCallback(self, msg):
+        enc = msg.data
+        if (enc < self.encoder_low_wrap and self.prev_lencoder > self.encoder_high_wrap):
+            self.lmult = self.lmult + 1
+            
+        if (enc > self.encoder_high_wrap and self.prev_lencoder < self.encoder_low_wrap):
+            self.lmult = self.lmult - 1
+            
+        self.left = 1.0 * (enc + self.lmult * (self.encoder_max - self.encoder_min)) 
+
+
+        self.prev_lencoder = enc
+        
+    def rwheelCallback(self, msg):
+        enc = msg.data
+        if(enc < self.encoder_low_wrap and self.prev_rencoder > self.encoder_high_wrap):
+            self.rmult = self.rmult + 1
+        
+        if(enc > self.encoder_high_wrap and self.prev_rencoder < self.encoder_low_wrap):
+            self.rmult = self.rmult - 1
+            
+        self.right = 1.0 * (enc + self.rmult * (self.encoder_max - self.encoder_min))
+
+
+        self.prev_rencoder = enc
+
+if __name__ == '__main__':
+    """ main """
+    diffTf = DiffTf()
+    diffTf.spin()
